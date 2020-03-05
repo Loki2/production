d690d5a7b3d661041e66826e8b5cb29223d629bc
loki <stevloki@ymail.com>
2020-03-05 16:18:49 +0700

    Updates

---
+++ precache-manifest.933fedb81c51a7986458c8931087978c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae3e7ff3e873fcbb8941371b82075c4",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "306ef34c7ac9f73a9da9",
+    "url": "/production/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89407d1aa573615e5810",
+    "url": "/production/static/js/2.dc80a3ab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/production/static/js/2.dc80a3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306ef34c7ac9f73a9da9",
+    "url": "/production/static/js/main.3d373848.chunk.js"
+  },
+  {
+    "revision": "bfa12c4be64209e883ea",
+    "url": "/production/static/js/runtime-main.9d9d9ca4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/production/static/media/logo.5d5d9eef.svg"
+  }
+]);
